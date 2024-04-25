0daee0b9eab2bdf39d7d4c91ea4dfe53f5d4c2a2
Sevlow <65291271+SevlowDev@users.noreply.github.com>
2024-04-25 21:24:05 +0200

    Update topbox.js

--- topbox.js
+++ topbox.js
@@ -1,3 +1,3 @@
 
 document.body.innerHTML = "<div class=\"pinned\" id=\"map\"><a href=\"/\">Home</a> | <a href=\"/projects\">Projects</a>  </div><br>" + document.body.innerHTML + 
-"</div><br><br><br>‎<div><br><footer id=\"footer\"><br>Made by Sevlow<br>2024<br>‎ </footer>";
+"</div><br><br><br>‎<div><br><footer id=\"footer\"><br>Made by Sevlow<br>2023-2024<br>‎ </footer>";
